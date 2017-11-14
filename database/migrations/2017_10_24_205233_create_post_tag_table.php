d0dc9870006ff8058f0734a9aeda3689a4f54ffc
Rob Lindsey <rclinds@icloud.com>
2017-11-14 16:24:08 -0500

    Fixed migration file class names

--- database/migrations/2017_10_24_205233_create_post_tag_table.php
+++ database/migrations/2017_10_24_205233_create_post_tag_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreatePostsTagsTable extends Migration
+class CreatePostTagTable extends Migration
 {
     /**
      * Run the migrations.
