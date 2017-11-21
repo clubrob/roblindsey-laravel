b292eb66e0ca2f2741f4ba1b3ab29df52034b586
Rob Lindsey <rclinds@icloud.com>
2017-11-21 15:04:26 -0500

    Fix FB share URL.

--- resources/views/post/show.blade.php
+++ resources/views/post/show.blade.php
@@ -42,7 +42,7 @@
                         <div>
                             <a class="twitter-share-button" href="https://twitter.com/intent/tweet?text={{ $post->title }}">Tweet</a>
                         </div>
-                        <div class="fb-share-button" data-href="https://developers.facebook.com/docs/plugins/" data-layout="button" data-size="small" data-mobile-iframe="true">
+                        <div class="fb-share-button" data-href="https://roblindsey.com/blog/post/{{$post->slug}}" data-layout="button" data-size="small" data-mobile-iframe="true">
                             <a class="fb-xfbml-parse-ignore" target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fdevelopers.facebook.com%2Fdocs%2Fplugins%2F&amp;src=sdkpreparse">Share</a>
                         </div>
                         <div class="linkedin-share-button">
