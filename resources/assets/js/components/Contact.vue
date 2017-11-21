916cb66fd081b39adc8cf8c2170a23e7d11695ec
Rob Lindsey <rclinds@icloud.com>
2017-11-21 15:53:24 -0500

    Tweaks for Contact form.

--- resources/assets/js/components/Contact.vue
+++ resources/assets/js/components/Contact.vue
@@ -4,23 +4,24 @@
             <h2 class="section-title">Contact</h2>
             <modal v-bind:message="message" v-if="modaltoggle" @close="changeValue"></modal>
             <p>Drop me a note to talk about your business project or just to say hi. I'll get back to you within a day usually, and we can hash it out. Don't be shy; say "Hi"!</p>
+            <p class="small"><span class="required">*</span> Required</p>
             <div class="row">
                 <form method="post" action="/contact" class="form contact-form">
                     <div class="form-element">
-                        <label for="name">Name</label>
+                        <label for="name"><span class="required" title="required">*</span>Name</label>
                         <input type="text" name="name" class="name-input" required>
                     </div>
                     <div class="form-element">
-                        <label for="email">Email Address</label>
+                        <label for="email"><span class="required" title="required">*</span>Email Address</label>
                         <input type="email" name="email" class="email-input" required>
                     </div>
                     <div class="form-element">
-                        <label for="phone">Phone Number</label>
-                        <input type="text" name="phone" class="phone-input" required>
+                        <label for="phone"><span class="required" title="required">*</span>Phone Number</label>
+                        <input type="tel" name="phone" class="phone-input" required>
                     </div>
                     <div class="form-element">
-                        <label for="message">Message</label>
-                        <textarea class="message-input" name="message" cols="30" rows="10" required></textarea>
+                        <label for="message"><span class="required" title="required">*</span>Message</label>
+                        <textarea class="message-input" name="message" cols="30" rows="10" maxlength="500" required></textarea>
                     </div>
                     <div class="form-element">
                         <button class="submit-button">Send Message</button>
