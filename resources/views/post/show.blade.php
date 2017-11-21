4a5208beb8cf6fa9688c5cf40dc4a36b554e85b0
Rob Lindsey <rclinds@icloud.com>
2017-11-21 15:01:33 -0500

    Adding social share buttons to blog posts.

--- resources/views/post/show.blade.php
+++ resources/views/post/show.blade.php
@@ -2,6 +2,16 @@
 
 @section ('content')
 
+{{--  Facebook Code  --}}
+    <div id="fb-root"></div>
+    <script>(function(d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s); js.id = id;
+    js.src = 'https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.11&appId=269164973103808';
+    fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));</script>
+
 <div id="app">
     <blognav></blognav>
     <div class="page-sig">
@@ -25,6 +35,21 @@
                     <div class="full-body">
                         {!! Markdown::convertToHtml($post->detail->body) !!}
                     </div>
+                    <div class="social">
+                        <div>
+                            <h5>Share:</h5>
+                        </div>
+                        <div>
+                            <a class="twitter-share-button" href="https://twitter.com/intent/tweet?text={{ $post->title }}">Tweet</a>
+                        </div>
+                        <div class="fb-share-button" data-href="https://developers.facebook.com/docs/plugins/" data-layout="button" data-size="small" data-mobile-iframe="true">
+                            <a class="fb-xfbml-parse-ignore" target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fdevelopers.facebook.com%2Fdocs%2Fplugins%2F&amp;src=sdkpreparse">Share</a>
+                        </div>
+                        <div class="linkedin-share-button">
+                            <script src="//platform.linkedin.com/in.js" type="text/javascript"> lang: en_US</script>
+                            <script type="IN/Share"></script>
+                        </div>
+                    </div>
                     <div class="tag-wrap">
                         <h5>Tags</h5>
                         <div class="tags">
@@ -43,6 +68,24 @@
 </div>
 
     @include('layouts/footer')
+    
+    {{--  Twitter Code  --}}
+    <script>window.twttr = (function(d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0],
+        t = window.twttr || {};
+    if (d.getElementById(id)) return t;
+    js = d.createElement(s);
+    js.id = id;
+    js.src = "https://platform.twitter.com/widgets.js";
+    fjs.parentNode.insertBefore(js, fjs);
+
+    t._e = [];
+    t.ready = function(f) {
+        t._e.push(f);
+    };
+
+    return t;
+    }(document, "script", "twitter-wjs"));</script>
 
     <script src="/js/app.js"></script>
 
